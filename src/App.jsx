89d35557e51da5154e08f5af8ee5b0902a7faa9c
khemraj-studentambassador <131950684+khemraj-studentambassador@users.noreply.github.com>
2023-04-28 13:15:53 +0000

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -24,9 +24,9 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "Khemraj Shrestha",
+  title: "Web Designer and Software developer",
+  email: "Khemraj.Shrestha@studentambassadors.com",
   gitHub: "microsoft",
   instagram: "microsoft",
   linkedIn: "satyanadella",
